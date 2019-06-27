14391c8c132ba02b7ab7ca76da2098dc70ed3c16
sunx.xiaojing <sun.xiaojing@yottabyte.cn>
2019-06-27 15:41:24 +0800

    修改删除方式

--- src/test/java/com/yottabyte/stepDefs/SetKeyWithValue.java
+++ src/test/java/com/yottabyte/stepDefs/SetKeyWithValue.java
@@ -67,7 +67,10 @@ public class SetKeyWithValue {
     @And("^I set the parameter \"([^割]*)\" to json editor$")
     public void iSetTheParameterToJsonEditor(String value) {
         WebElement element = webDriver.findElement(By.xpath("(//*[@class='ace_text-input'])[last()]"));
-        element.sendKeys(Keys.COMMAND, "a");
+        if (System.getProperty("os.name").startsWith("Windows"))
+            element.sendKeys(Keys.CONTROL, "a");
+        else
+            element.sendKeys(Keys.COMMAND, "a");
         element.sendKeys(Keys.DELETE);
         element.sendKeys(value);
     }
