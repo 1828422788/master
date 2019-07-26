558f7d00dfe071745e81585ad93f22213278cdb2
sunx.xiaojing <sun.xiaojing@yottabyte.cn>
2019-07-26 16:11:43 +0800

    修改元素等待时间

--- src/test/java/com/yottabyte/constants/WebDriverConst.java
+++ src/test/java/com/yottabyte/constants/WebDriverConst.java
@@ -12,7 +12,7 @@ public class WebDriverConst {
     public static final int WAIT_FOR_ELEMENT_TIMEOUT_WHEN_PAGE_LOADING = 5 * 1000;
 
     // 等待异步加载的元素的超时时间
-    public static final int WAIT_FOR_ELEMENT_LOADED_ASYNC_TIMEOUT = 180 * 1000;
+    public static final int WAIT_FOR_ELEMENT_LOADED_ASYNC_TIMEOUT = 200 * 1000;
 
     // 等待页面加载完毕
     public static final int WAIT_FOR_DOM_READY_TIMEOUT = 200 * 1000;
