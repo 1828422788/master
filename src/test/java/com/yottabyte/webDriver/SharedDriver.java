722674c5a303663f61809e4a4becc771dc973674
sunx.xiaojing <sun.xiaojing@yottabyte.cn>
2020-03-30 16:37:04 +0800

    测试webdriver

--- src/test/java/com/yottabyte/webDriver/SharedDriver.java
+++ src/test/java/com/yottabyte/webDriver/SharedDriver.java
@@ -173,7 +173,7 @@ public class SharedDriver extends EventFiringWebDriver {
 //            if ("Mac OS X".equalsIgnoreCase(System.getProperty("os.name"))) {
 //            options.setBinary(config.get("macbinary"));
 //            options.setBinary("C:\\Users\\Administrator\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe");
-            options.setBinary("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
+            options.setBinary("C:\\Program Files (x86)\\Google\\Chrome\\Application\\dfs\\chrome.exe");
 //            System.out.println("测试位置：" + config.get("macbinary"));
 //            }
             HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
