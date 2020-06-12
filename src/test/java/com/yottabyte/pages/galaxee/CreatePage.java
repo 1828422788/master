de9b30dd6d459beff9a002bc32af4be393ce144e
chen.shuang <chen.shuang@yottabyte.cn>
2020-06-12 13:48:31 +0800

    隐藏Chrome正在受到自动软件的控制

--- src/test/java/com/yottabyte/pages/galaxee/CreatePage.java
+++ src/test/java/com/yottabyte/pages/galaxee/CreatePage.java
@@ -21,8 +21,8 @@ import java.util.List;
 public class CreatePage extends PageTemplate {
     public CreatePage(WebDriver driver) {
         super(driver);
-//        driver.manage().window().setPosition(new Point(0,0));
-//        driver.manage().window().fullscreen();
+        driver.manage().window().setPosition(new Point(0,0));
+        driver.manage().window().fullscreen();
     }
 
     @FindBy(className = "el-input__inner")
