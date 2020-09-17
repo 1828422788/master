53d7d089e5a234c2d6da69d9df9d17ac2922d5d3
sunx.xiaojing <sun.xiaojing@yottabyte.cn>
2020-09-17 18:53:21 +0800

    修改switch操作

--- src/test/java/com/yottabyte/stepDefs/SwitchButton.java
+++ src/test/java/com/yottabyte/stepDefs/SwitchButton.java
@@ -1,47 +1,39 @@
 package com.yottabyte.stepDefs;
 
+import com.yottabyte.hooks.LoginBeforeAllTests;
 import com.yottabyte.utils.GetElementFromPage;
 import cucumber.api.java.en.And;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 /**
  * switch button相关操作
  */
 public class SwitchButton {
+    WebDriver webDriver = LoginBeforeAllTests.getWebDriver();
+
     /**
      * 将switch button置为disable/enable
      *
      * @param buttonName 元素名称
-     * @param value      disable/enable
+     * @param status     disable/enable
      */
     @And("^I switch the \"([^\"]*)\" button to \"([^\"]*)\"$")
-    public void iSwitchTheButtonTo(String buttonName, String value) {
-        WebElement element = GetElementFromPage.getWebElementWithName(buttonName);
-        WebElement enable = element.findElement(By.className("el-switch__label--left"));
-        WebElement disable = element.findElement(By.className("el-switch__label--right"));
-        if (value.equalsIgnoreCase("enable")) {
-            if (enable.getAttribute("style").contains("display: none")) {
-                element.click();
-            } else {
-                System.out.println("already enable");
-            }
-        } else if (value.equalsIgnoreCase("disable")) {
-            if (disable.getAttribute("style").contains("display: none")) {
-                element.click();
-            } else {
-                System.out.println("already disable");
-            }
-        } else {
-            System.out.println("error value :" + value);
+    public void iSwitchTheButtonTo(String buttonName, String status) {
+        String xpath = "//label[text()='" + buttonName + "']/following-sibling::button";
+        WebElement switchButton = webDriver.findElement(By.xpath(xpath));
+        String attribute = switchButton.getAttribute("class");
+        if (attribute.contains("checked") && "unchecked".equals(status) || !attribute.contains("checked") && "checked".equals(status)) {
+            switchButton.click();
         }
     }
 
     /**
      * 关闭或开启仪表盘开关
      *
-     * @param buttonName  元素名称
-     * @param value    状态 enable/disable
+     * @param buttonName 元素名称
+     * @param value      状态 enable/disable
      */
     @And("^I switch the dashboard \"([^\"]*)\" button to \"([^\"]*)\"$")
     public void operateDashboardSwitch(String buttonName, String value) {
