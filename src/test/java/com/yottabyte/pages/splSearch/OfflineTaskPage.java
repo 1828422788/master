2c7484aedd71989c0338788ac6f2303d039c813d
chen.shuang <chen.shuang@yottabyte.cn>
2020-10-20 21:06:57 +0800

    下载任务更新

--- src/test/java/com/yottabyte/pages/splSearch/OfflineTaskPage.java
+++ src/test/java/com/yottabyte/pages/splSearch/OfflineTaskPage.java
@@ -20,29 +20,19 @@ public class OfflineTaskPage extends PageTemplate {
         driver.manage().window().fullscreen();
     }
 
-    @FindBy(className = "el-table_1_column_1")
-    private List<WebElement> tdList;
+//    @FindBy(className = "el-table_1_column_1")
+//    private List<WebElement> tdList;
+//
+//    @FindBy(xpath = "//td[@class='el-table_1_column_1']")
+//    private WebElement emptyElement;
 
-    @FindBy(xpath = "//td[@class='el-table_1_column_1']")
-    private WebElement emptyElement;
+//    public WebElement getEmptyElement() {
+//        return emptyElement;
+//    }
 
-    @FindBy(xpath = "//a[contains(text(),'删除')]")
-    private WebElement delDownLoadButton;
-    public WebElement getDelDownLoadButton() {
-        return delDownLoadButton;
-    }
-
-    //@FindBy(className = "el-button--primary")
-    @FindBy(xpath = "//span[contains(text(),'确定')]/parent::button")
-    private WebElement ensureButton;
-
-    public WebElement getEmptyElement() {
-        return emptyElement;
-    }
-
-    public WebElement getFirstData() {
-        return tdList.get(1);
-    }
+//    public WebElement getFirstData() {
+//        return tdList.get(1);
+//    }
 
     public WebElement getErrorMessage() {
         return super.getErrorMessage();
@@ -52,19 +42,39 @@ public class OfflineTaskPage extends PageTemplate {
         return super.getSuccessMessage();
     }
 
-    public WebElement getEnsureButton() {
-        return ensureButton;
-    }
-
     public WebElement getSearch() {
         return super.getSearchIcon();
     }
 
-//    @FindBy(xpath = "//input[@placeholder='请输入名称']")
-//    private WebElement alertListSearchInput;
-//    public WebElement getAlertListSearchInput() {
-//        return alertListSearchInput;
+//    @Override
+//    protected void isLoaded() throws Error {
+//        WebDriverWait wait = new WebDriverWait(webDriver, 10, 1000);
+//        wait.until(new ExpectedCondition<Boolean>() {
+//            @Override
+//            public Boolean apply(WebDriver driver) {
+//                return driver.findElements(By.className("el-table_1_column_1")).size() > 1;
+//            }
+//        });
 //    }
+
+    @FindBy(xpath = "//a[contains(text(),'删除')]")
+    private WebElement delDownLoadButton;
+
+    public WebElement getDelDownLoadButton() {
+        return delDownLoadButton;
+    }
+
+    public WebElement getEnsureAfterDelDownload() {
+        return super.getButton("确定");
+    }
+
+    //@FindBy(className = "el-button--primary")
+    @FindBy(xpath = "//span[contains(text(),'确定')]/parent::button")
+    private WebElement ensureButton;
+
+    public WebElement getEnsureButton() {
+        return ensureButton;
+    }
 
     @FindBy(xpath = "//input[@placeholder='请输入名称']")
     private WebElement dbListPageSearchInput;
@@ -73,14 +83,4 @@ public class OfflineTaskPage extends PageTemplate {
         return dbListPageSearchInput;
     }
 
-    @Override
-    protected void isLoaded() throws Error {
-        WebDriverWait wait = new WebDriverWait(webDriver, 10, 1000);
-        wait.until(new ExpectedCondition<Boolean>() {
-            @Override
-            public Boolean apply(WebDriver driver) {
-                return driver.findElements(By.className("el-table_1_column_1")).size() > 1;
-            }
-        });
-    }
 }
