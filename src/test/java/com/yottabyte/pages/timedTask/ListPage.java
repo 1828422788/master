390396cc74a628eb191e3a2924c7bf86c4526950
ekaterina <ekaterina.kiseleva@yottabyte.cn>
2020-11-30 17:51:46 +0800

    修改定时任务用例

--- src/test/java/com/yottabyte/pages/timedTask/ListPage.java
+++ src/test/java/com/yottabyte/pages/timedTask/ListPage.java
@@ -14,6 +14,7 @@ public class ListPage extends ListPageFactory {
 
     public ListPage(WebDriver driver) {
         super(driver);
+        driver.manage().window().fullscreen();
     }
 
     @FindBy(xpath = "//*[contains(text(),'编辑')]")
